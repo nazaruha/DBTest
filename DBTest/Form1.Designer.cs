323d0dc110093b528ec1e9effd8edf720fe1454b
Nazar <fedunnazar20122001@gmail.com>
2022-07-06 16:07:51 +0300

    download NuGet package 'System.Data.SqlClient' v4.8.3

--- DBTest/Form1.Designer.cs
+++ DBTest/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(865, 536);
+            this.ClientSize = new System.Drawing.Size(897, 559);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
