60c42b958ec366ed7253a827a905199924d877ba
Nazar <fedunnazar20122001@gmail.com>
2022-07-08 18:25:55 +0300

    fix tblUserAddresses.sql

--- DBTest/SqlTables/tblUserAddresses.sql
+++ DBTest/SqlTables/tblUserAddresses.sql
@@ -1,17 +1,17 @@
-IF NOT EXISTS (SELECT * FROM sys.tables WHERE object_id = OBJECT_ID(N'[dbo].[tblUserAdresses]'))
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE object_id = OBJECT_ID(N'[dbo].[tblUserAddresses]'))
 EXEC dbo.sp_executesql @statement = N'
-CREATE TABLE [dbo].[tblUserAdresses](
+CREATE TABLE [dbo].[tblUserAddresses](
   [UserId] [int] NOT NULL,
   [CityId] [int] NOT NULL,
   [Street] [nvarchar](150) NOT NULL,
   [HouseNumber] [nvarchar](50) NOT NULL,
 
- CONSTRAINT [PK_tblUserAdresses] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_tblUserAddresses] PRIMARY KEY CLUSTERED 
  (  [UserId] ASC ),
 
- CONSTRAINT [FK_tblUserAdresses_tblUsers] FOREIGN KEY([UserId])
+ CONSTRAINT [FK_tblUserAddresses_tblUsers] FOREIGN KEY([UserId])
   REFERENCES [dbo].[tblUsers] ([Id]),
 
- CONSTRAINT [FK_tblUserAdresses_tblCities] FOREIGN KEY([CityId])
+ CONSTRAINT [FK_tblUserAddresses_tblCities] FOREIGN KEY([CityId])
   REFERENCES [dbo].[tblCities] ([Id])
 );'
